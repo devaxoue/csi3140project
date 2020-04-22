c0931705bc15938e283833d14fab3f3f6b4d3a96
Axel El <aelba103@uottawa.ca>
2020-04-22 07:54:19 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,6 @@ import "@firebase/firestore";
 
 // firebase init goes here
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCgiYWn_BJL1k_WqJHE3_gbfpPZFXroqfQ",
-  authDomain: "cookbook-61d9.firebaseapp.com",
-  databaseURL: "https://cookbook-61d9.firebaseio.com",
-  projectId: "cookbook-61d9",
-  storageBucket: "cookbook-61d9.appspot.com",
-  messagingSenderId: "214688260819"
 });
 
 export const db = firebaseApp.firestore();
